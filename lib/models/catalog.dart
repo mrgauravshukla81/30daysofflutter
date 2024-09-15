f5f11ae56418a6724d7cf8bb15f245988e30f828
Gaurav shukla <mrgauravshukla81@users.noreply.github.com>
2024-09-15 14:27:37 +0530

    Day10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+final products = [
+  Item(
+    id:"codepur001",
+   name:"iphone 12 Pro",
+    desc:"Apple iphone 12 generation",
+   price:999,
+    color:"#33505a",
+     image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRLiR_ixdgZJyfZ3Lu5bI9d8WaDo22AZvQRxX0CR5ZX9MUj6H9K"),
+];
